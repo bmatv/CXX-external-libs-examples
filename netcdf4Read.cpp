a5667532043626c6218828bab8abc63a40e3d9c5
Bogdan Matviichuk <hammermolotok@gmail.com>
2024-06-12 10:08:13 +1000

    extra attribute analysis added

--- netcdf4Read.cpp
+++ netcdf4Read.cpp
@@ -9,9 +9,16 @@ int main(){
     int ncid, varid,dimid;
 
 //    int data_in[NX][NY];
+//    std::string filepath;
+//    std::cin >> filepath;
+//    std::cout << filepath << '\n';
+//    auto fp = "dfsdfsdf";
 
     // Open the file and check to make sure it's valid.
-    netCDF::NcFile dataFile("simple_xy.nc", netCDF::NcFile::read);
+
+    auto filepath = "/home/bogdan/data/containerSamples/RSES_Wood_PigTeeth_3rdMolars/tomoSliceZ-2__RMG.nc";
+
+    netCDF::NcFile dataFile(filepath, netCDF::NcFile::read);
 
     // There are a number of inquiry functions in netCDF which can be
     // used to learn about an unknown netCDF file. In this case we know
@@ -24,14 +31,14 @@ int main(){
     std::cout<<"there are "<<dataFile.getGroupCount()<<" groups"<<std::endl;
     std::cout<<"there are "<<dataFile.getTypeCount()<<" types"<<std::endl;
 
-    auto xdim = dataFile.getDim("x");
-
     auto dims = dataFile.getDims();
 
     for(auto& entry:dims){
         std::cout <<"second.getName() = "<< entry.second.getName() <<" first = "<< entry.first <<" second.getSize() "<<entry.second.getSize() <<'\n';
     }
 
-    std::cout<<"getDim returs "<<xdim.getName()<<std::endl;
-
+    auto attrs = dataFile.getAtts();
+    for(auto& [attrName,attrNcGroupAtt]:attrs){
+        std::cout<<attrName<<" " <<attrNcGroupAtt.getName()<<'\n';
+    }
 }
