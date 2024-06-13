c1e52cda06bc7c26699a1d586f4e99a227805203
Bogdan Matviichuk <32863407+bmatv@users.noreply.github.com>
2024-06-13 17:13:41 +1000

    working mirosample to read-in a tomo

--- netcdf4Read.cpp
+++ netcdf4Read.cpp
@@ -4,30 +4,29 @@
 #include <iostream>
 #include <netcdf>
 
+
 int main(){
     /* This will be the netCDF ID for the file and data variable. */
 
-    // auto filepath = "/home/bogdan/data/containerSamples/RSES_Wood_PigTeeth_3rdMolars/tomoSliceZ-2__RMG.nc";
-    auto filepath = "simple_xy.nc";
+    auto filepath = "/home/bogdan/data/containerSamples/RSES_Wood_PigTeeth_3rdMolars/tomoSliceZ-2__RMG.nc";
+    // auto filepath = "simple_xy.nc";
 
 
     netCDF::NcFile dataFile(filepath, netCDF::NcFile::read);
 
-    netCDF::NcVar tomodata = dataFile.getVar("data");
+    netCDF::NcVar tomodata = dataFile.getVar("tomo",netCDF::NcGroup::Current);
 
-    int data_out[6][12];
-    // short tomoIn[10];
-    tomodata.getVar(data_out);
+    std::cout << "there are "<< tomodata.getDimCount() <<" dimensions in the dataset\n";
+    
+    // std::vector<int> data_out(6*12); // segfault without the size
+    std::vector<u_int16_t> data_out(1*2722*2722); // segfault without the size
+    // int data_out[1*2722*2722] {};
+    // int data_out[6*12] {};
+    tomodata.getVar(data_out.data());
 
-    for (int x = 0; x < 6; x++)
+    for (int x = 2772*1300; x < 2772*1301; x++)
     {
-        for (int y = 0; y < 12; y++)
-        {
-            std::cout<< data_out[x][y]<<' ';
-            }
-
-        std::cout <<'\n';
-        }
-
+        std::cout<< data_out[x]<<' ';
+    }
 
 }
