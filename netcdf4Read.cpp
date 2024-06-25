a479a8282e4ba7ad38b4a1503831985504376e22
Bogdan Matviichuk <32863407+bmatv@users.noreply.github.com>
2024-06-25 17:26:52 +1000

    dumping progress

--- netcdf4Read.cpp
+++ netcdf4Read.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <netcdf>
 #include <cmath>
+#include <cstdint>
 
 #include <vector>
 
@@ -11,7 +12,7 @@ int main()
 {
     /* This will be the netCDF ID for the file and data variable. */
 
-    auto filepath = "/home/bogdan/data/containerSamples/RSES_Wood_PigTeeth_3rdMolars/tomoSliceZ-2__RMG.nc";
+    auto filepath = "/home/bogdanm/data/containerSamples/RSES_Wood_PigTeeth_3rdMolars/tomoSliceZ-2__RMG.nc";
 
     netCDF::NcFile dataFile(filepath, netCDF::NcFile::read);
 
@@ -50,7 +51,7 @@ int main()
     std::cout << "The total sum is: " << accumulator << '\n';
     std::cout << "The average is: " << accumulator / (row_size * col_size) << '\n';
 
-    const int avg_filter_size = 3;
+    const int avg_filter_size = 5;
     const int filter_offset = avg_filter_size / 2;
     // std::vector<uint16_t> smooth_arr((row_size - filter_offset * 2) * (col_size - filter_offset * 2),0);
     std::vector<uint16_t> smooth_arr(row_size * col_size,0);
@@ -80,9 +81,13 @@ int main()
     std::cout << "---Sobel---\n";
     std::vector<double> edges_arr(row_size * col_size,0);
     // overflows if int16
+    double maxSobel = 0;
+    uint64_t countNonZero = 0;
+    double nonZeroSum = 0;
+    double nonZeroSquaredSum = 0;
 
-    for (int i=2;i<row_size-filter_offset*2;++i){
-    for (int j=2;j<col_size-filter_offset*2;++j){
+    for (int i=filter_offset+1;i<row_size-(filter_offset+1);++i){
+    for (int j=filter_offset+1;j<col_size-(filter_offset+1);++j){
         auto gx = (smooth_arr[(i+1)*row_size + j-1] + 2*smooth_arr[i*row_size + j-1] + smooth_arr[(i-1)*row_size + j-1]
                 +
                 -smooth_arr[(i+1)*row_size + j+1] + -2*smooth_arr[i*row_size+j+1] + -smooth_arr[(i-1)*row_size+j+1]);
@@ -91,20 +96,119 @@ int main()
                 +
                 -smooth_arr[(i+1)*row_size+j+1] + -2*smooth_arr[(i+1)*row_size+j] + -smooth_arr[(i+1)*row_size+j-1]);
 
-        edges_arr[(i)*row_size+j] = pow( (gx*gx + gy*gy),0.5);
+        auto sobelVal = pow( (gx*gx + gy*gy),0.5);
 
+        // edges_arr[(i)*row_size+j] = sobelVal<15000?sobelVal:0; // then we could ignore the mask but need to check whether the value is Nan in case of float tomo
+
+        if (sobelVal != 0){
+            if (sobelVal>maxSobel){
+                maxSobel = sobelVal;
+            }
+            countNonZero++;
+            nonZeroSum+=sobelVal;
+            nonZeroSquaredSum+=sobelVal*sobelVal;
+
+            edges_arr[i*row_size+j] = sobelVal;
+
+        }
+
+
+        
     }
     }
 
+    std::cout << "Maximum sobel value = " << maxSobel << '\n';
+    std::cout << "NonZero values count: " << countNonZero << '\n';
+    std::cout << "Zero values count: " << (row_size * col_size) - countNonZero << '\n';
+    std::cout << "Average value: " << nonZeroSum/countNonZero << '\n';
 
-    for (int i=500;i<510;++i){
-    for (int j=500;j<510;++j){
-        std::cout << edges_arr[i*row_size + j] <<' ';
-    }
-    std::cout << '\n';}
+    auto meanSobelVal = nonZeroSum/countNonZero;
+    auto meanSumSquared = nonZeroSquaredSum/countNonZero;
+
+    auto stdSobel = pow(meanSumSquared - meanSobelVal*meanSobelVal,0.5); // approx std. good enough
+
+    std::cout << "STD value: " << stdSobel << '\n' << "Filter bounds:" << meanSobelVal-stdSobel << " to " << meanSobelVal+stdSobel << '\n';
 
 
     //TODO filter the edges, potentially with a hard cutoff
+    long long filteredSobelCount = 0;
+    long long iAvg {};
+    long long jAvg {};
+    long long iStd {};
+    long long jStd {};
+
+    // compute average edge i and j, and respective standard deviations. Those will be our center coordinate and search box dimensions.
+
+
+    for (int i=filter_offset+1;i<row_size-(filter_offset+1);++i)
+    for (int j=filter_offset+1;j<col_size-(filter_offset+1);++j){
+        if (edges_arr[i*row_size+j]>(meanSobelVal+stdSobel) || edges_arr[i*row_size+j] < meanSobelVal){
+                edges_arr[i*row_size+j] = 0;
+        }
+        else { // edge found
+            filteredSobelCount++;
+            iAvg+=i;
+            jAvg+=j;
+            iStd+=i*i;
+            jStd+=j*j;
+            }
+    }
+
+    iAvg = iAvg/filteredSobelCount;
+    jAvg = jAvg/filteredSobelCount;
+    iStd = pow(iStd/filteredSobelCount - iAvg,0.5);
+    jStd = pow(jStd/filteredSobelCount - jAvg,0.5);
+    std::cout << "Zero values count after filtering: " <<filteredSobelCount <<'\n';
+    std::cout << "Average i = " << iAvg <<"\nSTD i = "<< iStd <<'\n';
+    std::cout << "Average j = " << jAvg <<"\nSTD j = "<< jStd <<'\n';
+
+    // The question is: which metric to use for hough transform - points being +=1 if non-zero while could also add up intensities 
+    // (high intensity targets could produce anomalous signals that will overwhelm the transform)
+    // long long test = iAvg - iStd*0.1;
+
+
+
+    const double centreboxbounds = 1.0/10.0;
+    long long iSearchSize = iStd*centreboxbounds;
+    long long jSearchSize = jStd*centreboxbounds;
+
+    std::vector<uint64_t>searchBox(iSearchSize* jSearchSize,0);
+
+    std::cout << "centre search box coordinates (i): " << iAvg - iSearchSize/2 << " to " << iAvg + iSearchSize/2 <<'\n';
+    std::cout << "centre search box coordinates (j): " << jAvg - jSearchSize/2 << " to " << jAvg + jSearchSize/2 <<'\n';
+
+    for (int i=filter_offset+1;i<row_size-(filter_offset+1);++i) // for every pixel of the tomogram
+    for (int j=filter_offset+1;j<col_size-(filter_offset+1);++j){
+        if (edges_arr[i*row_size+j]!=0){ // real edge, compute distance from every centre point
+
+            for(uint64_t iCentre = iAvg - iSearchSize/2; iCentre < iAvg +iSearchSize/2;i++) 
+            for(uint64_t jCentre = jAvg - jSearchSize/2; jCentre < jAvg + jSearchSize/2;i++){
+                // consider a radius of 1200
+                searchBox[iCentre-iAvg]
+                    
+
+
+                }
+
+        }
+
+    }
+
+    // for (int i=filter_offset+1;i<row_size-(filter_offset+1);++i)
+    // for (int j=filter_offset+1;j<col_size-(filter_offset+1);++j){
+    
+    
+    // for (int i=380;i<410;++i){
+    // for (int j=380;j<410;++j){
+    //     std::cout << edges_arr[i*row_size + j] <<' ';
+    // }
+    // std::cout << '\n';}
+
+
+    // write out the resultant netcdf after sobel filtering?
+
+
+
 
     //TODO run hough transform for the edges left (non-zero) 
 }
