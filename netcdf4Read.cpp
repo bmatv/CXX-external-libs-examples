dec7e639f91ac8efbe09dc058bea78f8012319af
Bogdan Matviichuk <32863407+bmatv@users.noreply.github.com>
2024-07-03 17:22:20 +1000

    changing the read function - int16 casted to uint16

--- netcdf4Read.cpp
+++ netcdf4Read.cpp
@@ -14,26 +14,46 @@ int main()
 {
     /* This will be the netCDF ID for the file and data variable. */
 
-    auto filepath = "/home/bogdanm/data/containerSamples/RSES_Wood_PigTeeth_3rdMolars/tomoSliceZ-2__RMG.nc";
+    // auto filepath = "/home/bogdanm/data/containerSamples/RSES_Wood_PigTeeth_3rdMolars/tomoSliceZ-2__RMG.nc"; // inner: 1208, outer: 1271(?)
+    // auto filepath = "/home/bogdanm/data/containerSamples/RSES_Wood_Teeth_123_8mm/tomoSliceZ-7__R.nc"; // inner: 1097
+    // auto filepath = "/home/bogdanm/data/containerSamples/Whiting_5640_5mm_031114_preserved/tomoSliceZ-13__R.nc"; // inner: 1088
+    auto filepath = "/home/bogdanm/data/containerSamples/Whiting_5640_5mm_031114_Xe_dec/tomoSliceZ-12__R.nc";
+    // std::vector<int>radii {1215,1216,1217,1218,1219,1267,1268,1269,1270,1271,1272,1273}; //1195,1200,1205,1210,1215,1220
+    // std::vector<int>radii {1207,1208,1209,};
+
+    // std::vector<int>radii {1096,1097,1098,1099,1100,1101,1102};
+
+    std::vector<int>radii {868,869,870,};
+
+
 
     netCDF::NcFile dataFile(filepath, netCDF::NcFile::read);
 
     netCDF::NcVar tomodata = dataFile.getVar("tomo", netCDF::NcGroup::Current);
 
     std::cout << "there are " << tomodata.getDimCount() << " dimensions in the tomo dataset\n";
+    std::cout << "tomodata.getDim(0).getSize() " << tomodata.getDim(0).getSize() << "\n";
+    std::cout << "tomodata.getDim(1).getSize() " << tomodata.getDim(1).getSize() << "\n";
+    std::cout << "tomodata.getDim(2).getSize() " << tomodata.getDim(2).getSize() << "\n";
 
-    const int32_t row_size = 2722;
-    const int32_t col_size = 2722;
+
+
+    size_t z_size   = tomodata.getDim(0).getSize();
+    size_t row_size = tomodata.getDim(1).getSize();
+    size_t col_size = tomodata.getDim(2).getSize();
+
 
     const std::vector<size_t> &start{0, 0, 0};           // 3*4, 12 bytes
-    const std::vector<size_t> &count{1, row_size, col_size}; // 12 bytes
+    const std::vector<size_t> &count{z_size, row_size, col_size}; // 12 bytes
 
-    std::vector<uint16_t> arr(row_size * col_size);
+    std::vector<int16_t> vec_int16(row_size * col_size);
 
     // uint16_t data_out[8364144/2] {}; //check if 8192, 8388608 the actual pass is 8364032, but the limit is inconsistent
     // e.g. uint8_t data[8377999] {}; might pass and might throw a Segmentation fault
 
-    tomodata.getVar(start, count, arr.data()); // the data is in, it's a 2D array
+    tomodata.getVar(start, count, vec_int16.data()); // the data is in, it's a 2D array
+
+    auto arr = *reinterpret_cast<std::vector<uint16_t>*>(&vec_int16);
     //writing mask value as it is missing in the file
     for(auto& val:arr){
         val = val!=0?val:static_cast<uint16_t>(65535); 
@@ -57,8 +77,8 @@ int main()
     // std::cout << "The total sum is: " << accumulator << '\n';
     // std::cout << "The average is: " << accumulator / (row_size * col_size) << '\n';
 
-    const int32_t avg_filter_size = 5;
-    const int32_t filter_offset = avg_filter_size / 2;
+    const size_t avg_filter_size = 5;
+    const size_t filter_offset = avg_filter_size / 2;
     // std::vector<uint16_t> smooth_arr((row_size - filter_offset * 2) * (col_size - filter_offset * 2),0);
     std::vector<int32_t> smooth_arr(row_size * col_size,0);
     // uint16_t smooth_arr[row_size-filter_offset*2][col_size-filter_offset*2] {};
@@ -150,14 +170,14 @@ int main()
 
     // compute average edge i and j, and respective standard deviations. Those will be our center coordinate and search box dimensions.
 
-    int32_t jmin = col_size;
-    int32_t jmax = 0;
+    size_t jmin = col_size;
+    size_t jmax = 0;
 
-    int32_t imin = row_size;
-    int32_t imax = 0;
+    size_t imin = row_size;
+    size_t imax = 0;
 
-    for (int32_t i=filter_offset+1;i<row_size-(filter_offset+1);++i)
-    for (int32_t j=filter_offset+1;j<col_size-(filter_offset+1);++j){
+    for (size_t i=filter_offset+1;i<row_size-(filter_offset+1);++i)
+    for (size_t j=filter_offset+1;j<col_size-(filter_offset+1);++j){
         if (edges_arr[i*row_size+j]>(meanSobelVal+stdSobel) || edges_arr[i*row_size+j] < meanSobelVal){
                 edges_arr[i*row_size+j] = 0;
         }
@@ -193,9 +213,9 @@ int main()
     jAvg = (jmax+jmin)/2;
     std::cout << "Average i = " << iAvg <<"; STD i = "<< iStd <<'\n';
     std::cout << "Average j = " << jAvg <<"; STD j = "<< jStd <<'\n';
-    std::cout << "Overriding iAvg and jAvg values with averaged 1361 \n";
-    iAvg = (iAvg+1361)/2;
-    jAvg = (jAvg+1361)/2;
+    // std::cout << "Overriding iAvg and jAvg values with averaged 1361 \n";
+    // iAvg = (iAvg+static_cast<long long> (row_size)/2)/2;
+    // jAvg = (jAvg+static_cast<long long>(col_size)/2)/2;
 
     // The question is: which metric to use for hough transform - points being +=1 if non-zero while could also add up intensities 
     // (high intensity targets could produce anomalous signals that will overwhelm the transform)
@@ -209,7 +229,7 @@ int main()
     long long iSearchSize = 64; // has to be even otherwise not all values will be accesible
     long long jSearchSize = 64;
     float halfDiagLen = std::sqrt(static_cast<float>(iSearchSize*iSearchSize + jSearchSize*jSearchSize));
-    std::vector<int>radii {1215,1216,1217,1218,1219,1267,1268,1269,1270,1271,1272,1273}; //1195,1200,1205,1210,1215,1220
+
     std::vector<uint64_t>searchBox(iSearchSize*jSearchSize*radii.size(),0);
 
     std::cout << "centre search box coordinates (i): " << iAvg - iSearchSize/2 << " to " << iAvg + iSearchSize/2 << "\niSearchSize (Window) = " << iSearchSize <<'\n';
@@ -217,8 +237,8 @@ int main()
 
 
     // for every pixel of the tomogram
-    for (int i = filter_offset + 1; i < row_size - (filter_offset + 1); ++i)
-        for (int j = filter_offset + 1; j < col_size - (filter_offset + 1); ++j) {
+    for (int i = filter_offset + 1; i < static_cast<int>(row_size - (filter_offset + 1)); ++i)
+        for (int j = filter_offset + 1; j < static_cast<int>(col_size - (filter_offset + 1)); ++j) {
             // real edge, compute distance from every centre point
 
             if (edges_arr[i * row_size + j] != 0) {
@@ -246,7 +266,7 @@ int main()
                                               std::pow(static_cast<float>(j - jCentre), 2.0F))));
                                 long long indexI = 0;
                                 long long indexJ = 0;
-                                int32_t edgeStd = 2;
+                                int32_t edgeStd = 0;
                                 if (distance >= (radii[rIdx]-edgeStd) && distance <= (radii[rIdx]+edgeStd)) {
                                     indexI = iCentre - iAvg + iSearchSize / 2;
                                     indexJ = jCentre - jAvg + jSearchSize / 2;
@@ -266,15 +286,28 @@ int main()
     // find the maximum pixel value
     size_t idxMax = 0;
     size_t valMax = 0;
+    // for(size_t i=1+iSearchSize;i<searchBox.size()-iSearchSize;++i){
+    //     auto crossSum = searchBox.at(i-iSearchSize) + searchBox.at(i-1) + searchBox.at(i) + searchBox.at(i+1) + searchBox.at(i+iSearchSize);
+    //     if (valMax < crossSum){
+    //         valMax = crossSum;
+    //         idxMax = i;
+    //     }
+    // }
     for(size_t i=1+iSearchSize;i<searchBox.size()-iSearchSize;++i){
-        auto crossSum = searchBox.at(i-iSearchSize) + searchBox.at(i-1) + searchBox.at(i) + searchBox.at(i+1) + searchBox.at(i+iSearchSize);
-        if (valMax < crossSum){
-            valMax = crossSum;
+        // auto crossSum = searchBox.at(i-iSearchSize) + searchBox.at(i-1) + searchBox.at(i) + searchBox.at(i+1) + searchBox.at(i+iSearchSize);
+        if (valMax < searchBox.at(i)){
+            valMax = searchBox.at(i);
             idxMax = i;
         }
     }
-    std::cout << "MaxVal: " << valMax << "; MaxValIdx: " << idxMax <<'\n';
 
+
+    std::cout << "MaxVal: " << valMax << "; MaxValIdx: " << idxMax <<'\n';
+    std::cout << "Radius: " << radii.at(idxMax/(iSearchSize*iSearchSize)) << "; iIdx: " << (idxMax%(iSearchSize*iSearchSize))/iSearchSize
+    << "; jIdx: " << idxMax%iSearchSize<<'\n';
+    //get the actual coordinates
+    std::cout << "xAbs: " << idxMax%iSearchSize - iSearchSize/2 + jAvg << 
+    "; yAbs: " << (idxMax%(iSearchSize*iSearchSize))/iSearchSize - iSearchSize/2 + iAvg <<'\n';
     std::ofstream myfile;
     myfile.open ("example.txt");
     
